2825d0fe8ce98c29c4d72aa847dce0a9445a84fc
vinodsawant.azure@gmail.com <50289281+vinodswnt306@users.noreply.github.com>
2021-06-01 10:57:55 +0530

    'c111'

--- pipeline/pipeline_run.py
+++ pipeline/pipeline_run.py
@@ -7,3 +7,12 @@ client.create_run_from_pipeline_package(
     arguments = {'gcs_path': 'gs://bucket-306/data/train/dataloan.csv' },experiment_name='MLOps_prod'
     
 )
+
+
+
+import subprocess, sys, os
+
+command = subprocess.run(['git', 'log','-1', '--pretty=%h'], capture_output=True)
+print(command.stdout.decode('utf-8')[:-2])
+
+

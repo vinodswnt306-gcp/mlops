af00f015031f3fc15d5dcbe7836d4047c0eed259
vinodsawant.azure@gmail.com <50289281+vinodswnt306@users.noreply.github.com>
2021-06-01 14:08:26 +0530

    'c111'

--- file.sh
+++ file.sh
@@ -1,5 +1,5 @@
 export CONTAINER_NAME="$(git log -1 --pretty=%h)"
 docker login --username=vinodswnt306 --password=vinodsawant
 docker build -t vinodswnt306/new_public_mlops:$CONTAINER_NAME .
-docker push -t vinodswnt306/new_public_mlops:$CONTAINER_NAME
+docker push vinodswnt306/new_public_mlops:$CONTAINER_NAME
 

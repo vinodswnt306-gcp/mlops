8cd4c701da004f13ca6fa8420a54fa02c505e4ad
vinodsawant.azure@gmail.com <50289281+vinodswnt306@users.noreply.github.com>
2021-06-01 10:35:39 +0530

    'c112'

--- pipeline/pipeline_run.py
+++ pipeline/pipeline_run.py
@@ -5,4 +5,5 @@ client = kfp.Client('https://2886795272-31380-shadow05.environments.katacoda.com
 client.create_run_from_pipeline_package(
     pipeline_file='pipeline/ds_train.yaml',
     arguments = {'gcs_path': 'gs://bucket-306/data/train/dataloan.csv' },experiment_name='MLOps_prod'
+    
 )

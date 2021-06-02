0053d253f7fb25351d608cd948b3e02777a60d69
vinodsawant.azure@gmail.com <50289281+vinodswnt306@users.noreply.github.com>
2021-06-02 12:19:31 +0530

    'c2323'

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3.7-slim
-COPY ./requirements.txt ./docker/secrets.json ./
-COPY ./utils utils
-RUN pip install -r requirements.txt
-
--- pipeline_base_image_builder.sh
+++
@@ -1,5 +0,0 @@
-export CONTAINER_NAME="$(git log -1 --pretty=%h)"
-docker login --username=vinodswnt306 --password=vinodsawant
-docker build -t vinodswnt306/new_public_mlops:$CONTAINER_NAME -f ./docker/Dockerfile .
-docker push vinodswnt306/new_public_mlops:$CONTAINER_NAME
-

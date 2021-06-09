d40d283ffa9653f63d5b68ae1835c00303909540
vinodsawant.azure@gmail.com <vinodsawant.azure@gmail.com>
2021-06-09 17:18:49 +0530

    c22

--- pipeline/pipeline_run.py
+++ pipeline/pipeline_run.py
@@ -9,12 +9,6 @@ import pandas
 import gcsfs
 import pandas as pd
 
-# Connect to GCS and check current model version
-import gcsfs
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'secrets.json'
-fs = gcsfs.GCSFileSystem(project='leafy-ether-314809' , token='secrets.json',cache_timeout=0)
-initial_length = len(fs.ls('gs://loan_model_pipeline'))
-
 # Run kfp pipeline
 import kfp
 # Set up Kubeflow client using its url
@@ -23,15 +17,3 @@ run = client.create_run_from_pipeline_package(
         pipeline_file='pipeline/ds_train.yaml',
         arguments = {'gcs_path': 'gs://bucket-306/data/train/dataloan.csv' },experiment_name='MLOps_prod'
         )
-
-client.wait_for_run_completion(run.run_id, 3600)
-
-# If model was registered then save this pipeline to model's file
-new_length = len(fs.ls('gs://loan_model_pipeline'))
-if new_length > initial_length:
-    gcs_files = [i.replace('loan_model_pipeline/','') for i in fs.ls('gs://loan_model_pipeline/')]
-    folder_num = gcs_files[-1]
-    with open("pipeline/ds_train.yaml", "rb") as local_file:
-        with fs.open("gs://loan_model_pipeline/" + folder_num + "/model_pipeline.yaml", "wb") as gcs_file:
-            gcs_file.write(local_file.read())
-
